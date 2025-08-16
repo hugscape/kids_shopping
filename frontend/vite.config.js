955372c14b8dcf61b92306f419cb24599b9916c6
jaswanth <jaswanth.pappu@outlook.com>
2025-08-16 21:06:15 +0530

    base path in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/kids_shopping/' : '/',
+  base: '/', // Set base to '/' for custom domain deployment
   server: {
     port: 3000,
     proxy: {
